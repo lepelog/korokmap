1270aa8550eda88cdcebb38c172dabc9079ecb6f
oliver <o-lohr@gmx.de>
2019-01-15 01:26:50 +0100

    Differentiate koroks under rocks

--- generate/attach_types.py
+++ generate/attach_types.py
@@ -9,15 +9,19 @@ with open('rawtypes.csv') as csvf:
     rawtypes = list(csv.reader(csvf))
 
 # Get all types from rawtypes second column, set to distinct then sort and to list, disregard title
-allkoroktypes = list(sorted(set(x[1] for x in rawtypes[1:])))
 
 # map one csv row to dict member
 def rawtypetodict(rawtype):
     tid = "{:0>3}".format(rawtype[0])
+    # in case it's a rock korok differentiate
+    if (rawtype[1] == 'Lift the Rock' and rawtype[2] != ''):
+        # the comment is always 'Under' something
+        rawtype[1] = rawtype[1] + ' (' + rawtype[2][6:] + ')'
     return (tid, rawtype)
 
 dicttypes = dict(rawtypetodict(x) for x in rawtypes[1:])
 allkoroks = [{'type':dicttypes[x['id'][4:]][1], 'comment':dicttypes[x['id'][4:]][2], **x} for x in rawallkoroks]
+allkoroktypes = list(sorted(set(x['type'] for x in allkoroks)))
 with open('allkoroks.js', 'w') as resf:
     resf.write('const allkoroktypes = ')
     json.dump(allkoroktypes, resf)
